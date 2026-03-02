f06866d4ad5be0075445b927095698535788cc26
andresfelipecastellanos1-ui <andresfelipecastellanos1@gmail.com>
2026-03-02 08:17:55 -0500

    Replace temperature conversion with triangle validation

--- ejercicio_02/ejercicio_02.cpp
+++ ejercicio_02/ejercicio_02.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
-#include <iomanip>
-
 using namespace std;
 
 int main() {
-    double celsius;
-    double fahrenheit;
-    double kelvin;
-    double rankine;
+    float a, b, c;
     
-    cout << "Ingrese temperatura en Celsius: ";
-    cin >> celsius;
+    cout << "Ingresa lado 1: ";
+    cin >> a;
+    cout << "Ingresa lado 2: ";
+    cin >> b;
+    cout << "Ingresa lado 3: ";
+    cin >> c;
     
-    fahrenheit = (celsius * 9.0 / 5.0) + 32;
-    kelvin = celsius + 273.15;
-    rankine = (celsius + 273.15) * 9.0 / 5.0;
-    
-    cout << fixed << setprecision(2);
-    
-    cout << "Fahrenheit: " << fahrenheit << endl;
-    cout << "Kelvin: " << kelvin << endl;
-    cout << "Rankine: " << rankine << endl;
+    // Verificar si es triangulo valido
+    if (a + b > c && a + c > b && b + c > a) {
+        cout << "Es un triangulo valido" << endl;
+        
+        // Por lados
+        if (a == b && b == c) {
+            cout << "Equilatero" << endl;
+        } else if (a == b || b == c || a == c) {
+            cout << "Isosceles" << endl;
+        } else {
+            cout << "Escaleno" << endl;
+        }
+        
+        // Por angulos (usando pitagoras)
+        // Ordenamos para que 'c' sea el mayor
+        float aux;
+        if (a > b) { aux = a; a = b; b = aux; }
+        if (b > c) { aux = b; b = c; c = aux; }
+        if (a > b) { aux = a; a = b; b = aux; }
+        
+        // Ahora c es el lado mayor
+        if (c * c == a * a + b * b) {
+            cout << "Rectangulo" << endl;
+        } else if (c * c > a * a + b * b) {
+            cout << "Obtusangulo" << endl;
+        } else {
+            cout << "Acutangulo" << endl;
+        }
+        
+    } else {
+        cout << "No es un triangulo valido" << endl;
+    }
     
     return 0;
 }
