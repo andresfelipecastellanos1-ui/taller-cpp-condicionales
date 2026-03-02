25f0db38ca2451f6b9d032e5a8e6470f0de778f3
andresfelipecastellanos1-ui <andresfelipecastellanos1@gmail.com>
2026-03-02 08:12:15 -0500

    feat: resuelto ejercicio 01

---
+++ ejercicio_01/ejercicio_01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double celsius;
+    double fahrenheit;
+    double kelvin;
+    double rankie;
+
+    cout << "ingrese temperatura en Celsius: ";
+    cin >> celsius;
+
+    fahrenheit = (celsius * 9.0/5.0) + 32;
+    kelvin = celsius + 273.15;
+    rankie = (celsius + 273.15) * 9.0/5.0;
+
+    cout << "fahrenheit: " << fahrenheit << endl;
+    cout << "kelvin: " << kelvin << endl;
+    cout << "rankie: " << rankie << endl;
+
+    return 0;
+}
